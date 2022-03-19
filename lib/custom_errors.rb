7184cbf0dc10186ab18eb936658cddadd45f0e7e
Takashi <th2826@columbia.edu>
2022-03-19 18:34:32 -0400

    no issue

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -13,9 +13,11 @@ class Person
 end
 
 beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
+jz = Person.new("Jay-Z")
 
+beyonce.get_married(jz)
+puts beyonce.name
+puts beyonce.partner
 
 
 
